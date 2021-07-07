19b3333a8bd0a2495f7099ef8bc77cb51384493e
Maxim Gorovets <gorovec3@gmail.com>
2021-07-07 12:18:08 +0300

    add fibbonaci

---
+++ src/test/java/com/example/fibbonaci/FibbonaciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fibbonaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibbonaciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
