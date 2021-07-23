59224506fe99d3248d0d33b3fe5133b63cc13163
Maxim Gorovets <gorovec3@gmail.com>
2021-07-23 14:16:39 +0300

    Set TTL for cache

--- src/main/java/com/example/springfibonnaci/SpringfibonnaciApplication.java
+++ src/main/java/com/example/springfibonnaci/SpringfibonnaciApplication.java
@@ -2,9 +2,12 @@ package com.example.springfibonnaci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigInteger;
 
 @EnableCaching
+@EnableScheduling
 @Configuration
 @SpringBootApplication
 @RestController
@@ -24,6 +28,7 @@ public class SpringfibonnaciApplication {
     @GetMapping("/fibbonaci/{number}")
     @Cacheable(value = "number-cache", key = "'Number:' + #number")
     public String fibbonaci(@PathVariable("number") int number) throws InterruptedException {
+        Thread.sleep(5000);
         if (number == 0) {
             return BigInteger.ZERO.toString();
         }
@@ -36,5 +41,11 @@ public class SpringfibonnaciApplication {
         BigInteger nthFibonacci = powerOfA[0][0];
 
         return nthFibonacci.toString();
+    }
+
+    @CacheEvict(value = "number-cache", allEntries = true)
+    @Scheduled(fixedDelay = 600000)
+    public void cacheEvict() {
+
     }
 }
