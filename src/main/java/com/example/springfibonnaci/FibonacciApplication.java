c45dd4253ed755e1ef91caf2d8e3434b94aad324
Maxim Gorovets <gorovec3@gmail.com>
2021-07-28 15:37:11 +0300

    Add new import

--- src/main/java/com/example/springfibonnaci/FibonacciApplication.java
+++ src/main/java/com/example/springfibonnaci/FibonacciApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({CacheConfiguration.class, SсheduleConfugiration.class})
+@Import({CacheConfiguration.class,
+        SсheduleConfugiration.class,
+        CloudServerConfiguration.class
+})
 public class FibonacciApplication {
     public static void main(String[] args) {
         SpringApplication.run(FibonacciApplication.class, args);
