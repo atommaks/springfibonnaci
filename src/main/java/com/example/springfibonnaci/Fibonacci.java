9bf4006cd2c708a269901ce8aa41634d60314af5
Maxim Gorovets <gorovec3@gmail.com>
2021-08-06 14:02:08 +0300

    Updated Fibonacci.java

--- src/main/java/com/example/springfibonnaci/Fibonacci.java
+++ src/main/java/com/example/springfibonnaci/Fibonacci.java
@@ -1,11 +1,7 @@
 package com.example.springfibonnaci;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,48 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.math.BigInteger;
-
+import javax.naming.ConfigurationException;
 
 @RestController
-@RefreshScope
 public class Fibonacci {
     private static final String BAD_REQUEST_MESSAGE = "Please, enter n >= 0!\n";
 
-    @Value("${text.copyright: Default Copyright}")
-    private String copyright;
-
-    @Autowired
-    private Environment environment;
-
     @GetMapping("/fibbonaci/{number}")
     @Cacheable(value = "number-cache", key = "'Number:' + #number")
-    public String fibonacci(@PathVariable("number") int number) {
-        if (isTechProfileActive()) {
-            return copyright;
-        }
-
+    public String fibonacci(@PathVariable("number") int number) throws ConfigurationException {
         if (number < 0) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, BAD_REQUEST_MESSAGE);
         }
+        Service fibService = new ServiceConfiguration().fibonacciService();
 
-        BigInteger res = FibonacciService.countNthFib(number);
-
-        return res.toString();
+        return fibService.countNthFib(number);
     }
 
     @CacheEvict(value = "number-cache", allEntries = true)
     @Scheduled(fixedDelay = 600000)
     public void cacheEvict() {
 
-    }
-
-    private boolean isTechProfileActive() {
-        for (final String profileName : environment.getActiveProfiles()) {
-            if("tech".equals(profileName)){
-                return true;
-            }
-        }
-        return false;
     }
 }
