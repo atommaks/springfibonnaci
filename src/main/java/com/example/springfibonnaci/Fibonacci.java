6914249ea09f55887df435f9c87f16fc8923ebbd
Maxim Gorovets <gorovec3@gmail.com>
2021-08-06 15:58:28 +0300

    Updated Fibonacci.java

--- src/main/java/com/example/springfibonnaci/Fibonacci.java
+++ src/main/java/com/example/springfibonnaci/Fibonacci.java
@@ -1,7 +1,9 @@
 package com.example.springfibonnaci;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,21 +11,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.naming.ConfigurationException;
-
 @RestController
+@Configuration
 public class Fibonacci {
     private static final String BAD_REQUEST_MESSAGE = "Please, enter n >= 0!\n";
 
+    @Autowired
+    public Service service;
+
     @GetMapping("/fibbonaci/{number}")
     @Cacheable(value = "number-cache", key = "'Number:' + #number")
-    public String fibonacci(@PathVariable("number") int number) throws ConfigurationException {
+    public String fibonacci(@PathVariable("number") int number){
         if (number < 0) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, BAD_REQUEST_MESSAGE);
         }
-        Service fibService = new ServiceConfiguration().fibonacciService();
 
-        return fibService.countNthFib(number);
+        return service.countNthFib(number);
     }
 
     @CacheEvict(value = "number-cache", allEntries = true)
